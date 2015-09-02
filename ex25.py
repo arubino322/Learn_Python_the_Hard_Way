c60a6b567e235b51c7485025c29049466793b0aa
arubino322 <arubino322@gmail.com>
2015-09-02 11:49:53 -0400

    Added comments practicing with functions

--- ex25.py
+++ ex25.py
@@ -1,17 +1,17 @@
 def break_words(stuff):
-	"""This function will break up worlds for us."""
+	"""This function will break up words for us."""
 	words = stuff.split(' ')
 	return words
-
+#Note: sorting the words only works after you split it and turn the sentence string into a list
 def sort_words(words):
 	"""Sorts the words."""
 	return sorted(words)
-
+#sorts the list. If you sort the string sentence, it words the punctuations and letters
 def print_first_word(words):
 	"""Prints the first word after popping it off."""
 	word = words.pop(0)
 	print word
-
+#Again, .pop function only works for lists, no strings. It pops the word outta the list.
 def print_last_word(words):
 	"""Prints the last word after popping it off"""
 	word = words.pop(-1)
